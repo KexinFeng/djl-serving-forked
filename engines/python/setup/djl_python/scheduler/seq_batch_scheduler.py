e772b3c8f9ccce3e669410320a5be7783406f667
KexinFeng <fenkexin@amazon.com>
2023-05-16 17:07:34 -0700

    LMBlock, initForward and incrementForward for greedy and scheduler debugging (#743-2)

--- engines/python/setup/djl_python/scheduler/seq_batch_scheduler.py
+++ engines/python/setup/djl_python/scheduler/seq_batch_scheduler.py
@@ -1,15 +1,30 @@
+#!/usr/bin/env python
+#
+# Copyright 2023 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file
+# except in compliance with the License. A copy of the License is located at
+#
+# http://aws.amazon.com/apache2.0/
+#
+# or in the "LICENSE.txt" file accompanying this file. This file is distributed on an "AS IS"
+# BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, express or implied. See the License for
+# the specific language governing permissions and limitations under the License.
+
 from abc import ABC, abstractmethod
+from typing import List, Union
 
 import torch
 
 from djl_python.scheduler.search_config import SearchConfig
 from djl_python.scheduler.seq_batcher import SeqBatcher
+from djl_python.scheduler.lm_block import LMBlock
 
 
 class SeqBatchScheduler(ABC):
 
-    def __init__(self):
-        self.lm_block = None
+    def __init__(self, lm_block: LMBlock):
+        self.lm_block = lm_block
         self.results = {}
         self.seq_batcher = None
         self.config = SearchConfig()
@@ -17,30 +32,29 @@ class SeqBatchScheduler(ABC):
     @abstractmethod
     def init_forward(self, input_ids: torch.Tensor,
                      batch_uids: torch.Tensor,
-                     config: SearchConfig) -> SeqBatcher:
+                     config: SearchConfig,
+                     kv_cache: Union[List[torch.Tensor], None]) -> SeqBatcher:
         pass
 
     def increment_forward(self, count: int) -> torch.Tensor:
         i = 0
         intermediate = None
         while i < count:
-            if self.seq_batcher is None or self.seq_batcher.get_batch():
+            if self.seq_batcher is None or self.seq_batcher.get_batch() is None:
                 raise Exception("SeqBatcher not set. Please call add_batch."
                                 f"Current inference order is {i}")
 
-            intermediate = self.inference_call()
+            yield self.inference_call()
             if self.seq_batcher.seq_complete():
                 self.results.update(self.seq_batcher.collect_and_trim())
             i += 1
-
-        return intermediate
 
     @abstractmethod
     def inference_call(self) -> torch.Tensor:
         pass
 
-    def add_request(self, input_ids, batch_uids, config):
-        new_seq_batcher = self.init_forward(input_ids, batch_uids, config)
+    def add_request(self, input_ids, batch_uids, config, kv_cache=None):
+        new_seq_batcher = self.init_forward(input_ids, batch_uids, config, kv_cache)
         if self.seq_batcher:
             self.seq_batcher.add_batch(new_seq_batcher)
         else:
@@ -66,9 +80,9 @@ class SeqBatchScheduler(ABC):
                     break
                 index += 1
 
-            offsets[i] = index
+            offsets.append(index)
 
-        return torch.tensor(offsets, dtype=torch.int64)
+        return torch.tensor(offsets, dtype=torch.int64).view(-1, 1)
 
     @staticmethod
     def compute_attention_mask(input_ids, config):
@@ -95,13 +109,17 @@ class SeqBatchScheduler(ABC):
         return attention_mask
 
     @staticmethod
-    def compute_position_ids(input_ids: torch.Tensor, offsets: torch.Tensor,
-                             past_seq_len: int, repeat: int):
-        position_ids = torch.repeat_interleave(torch.unsqueeze(torch.arange(
+    def compute_position_ids(input_ids: torch.Tensor,
+                             offsets: torch.Tensor,
+                             past_seq_len: int,
+                             repeat: int):
+        position_range = torch.arange(
             start=past_seq_len,
             end=past_seq_len + input_ids.shape[-1],
             step=1,
-            dtype=torch.int64),
+            dtype=torch.int64)
+
+        position_ids = torch.repeat_interleave(torch.unsqueeze(position_range,
             dim=0),
             dim=0,
             repeats=input_ids.shape[0])
