44f204f38f14f81c9a64e9c1b7ea2f1a86df2205
KexinFeng <fenkexin@amazon.com>
2023-05-16 17:07:34 -0700

    LMBlock, initForward and incrementForward for greedy and scheduler debugging (#743-2)

--- engines/python/setup/djl_python/scheduler/contrastive_batch.py
+++ engines/python/setup/djl_python/scheduler/contrastive_batch.py
@@ -1,8 +1,9 @@
+from __future__ import annotations
+
 from typing import List
 
 import torch
-from utils import merge_tensors
-from __future__ import annotations
+from scheduler.static_methods import merge_tensors
 
 from djl_python.scheduler.batch import Batch
 
@@ -11,41 +12,31 @@ PAD_TOKEN_ID = 220
 
 class ContrastiveBatch(Batch):
 
-    def __init__(
-        self,
-        seq_dim_order: List[int],
-        past_output_ids: torch.Tensor = None,
-        past_attention_mask: torch.Tensor = None,
-        past_hidden_states: torch.Tensor = None,
-        logits: torch.Tensor = None,
-        past_key_values: List[torch.Tensor] = None,
-    ):
-        self.seq_dim_order = seq_dim_order
-        self.past_output_ids = past_output_ids
-        self.past_attention_mask = past_attention_mask
+    def __init__(self, seq_dim_order: List[int], past_output_ids: torch.Tensor = None,
+                 past_attention_mask: torch.Tensor = None, past_hidden_states: torch.Tensor = None,
+                 logits: torch.Tensor = None, past_key_values: List[torch.Tensor] = None):
+        super().__init__(seq_dim_order, past_output_ids, past_attention_mask, logits, past_key_values)
         self.past_hidden_states = past_hidden_states
-        self.past_key_values = past_key_values
-        self.logits = logits
 
     def merge(self, batch: ContrastiveBatch, seq_delta):
         past_output_ids = merge_tensors(self.past_output_ids,
                                         batch.past_output_ids,
                                         seq_delta=seq_delta,
-                                        seq_order=self.seq_dim_order[0],
+                                        seq_order=1,
                                         is_pad_token=True)
         past_attention_mask = merge_tensors(self.past_attention_mask,
                                             batch.past_attention_mask,
                                             seq_delta=seq_delta,
-                                            seq_order=self.seq_dim_order[1])
+                                            seq_order=1)
 
         past_hidden_states = merge_tensors(self.past_hidden_states,
                                            batch.past_hidden_states,
                                            seq_delta=seq_delta,
-                                           seq_order=self.seq_dim_order[2])
+                                           seq_order=1)
         logits = merge_tensors(self.logits,
                                batch.logits,
                                seq_delta=seq_delta,
-                               seq_order=self.seq_dim_order[3])
+                               seq_order=-1)
 
         past_key_values = []
         for i in range(len(self.past_key_values)):
@@ -53,7 +44,7 @@ class ContrastiveBatch(Batch):
                 merge_tensors(self.past_key_values[i],
                               batch.past_key_values[i],
                               seq_delta=seq_delta,
-                              seq_order=self.seq_dim_order[i]))
+                              seq_order=2))
 
         return ContrastiveBatch(seq_dim_order=self.seq_dim_order,
                                 past_output_ids=past_output_ids,
@@ -69,3 +60,6 @@ class ContrastiveBatch(Batch):
 
     def trim(self, trim_sequence: int, keep_indices: List[int]):
         pass
+
+
+
