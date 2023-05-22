71f66006a38d46ccc858ccec8d9cc6d7f75bf11d
Sindhu Somasundaram <56774226+sindhuvahinis@users.noreply.github.com>
2023-05-22 16:32:28 -0700

    Skeleton structure for sequence batch scheduler (#745)
    * Skeleton structure for rolling batch scheduler
    
    * Adding license header

--- engines/python/setup/djl_python/scheduler/contrastive_batch.py
+++ engines/python/setup/djl_python/scheduler/contrastive_batch.py
@@ -1,9 +1,19 @@
-from __future__ import annotations
-
+#!/usr/bin/env python
+#
+# Copyright 2023 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file
+# except in compliance with the License. A copy of the License is located at
+#
+# http://aws.amazon.com/apache2.0/
+#
+# or in the "LICENSE.txt" file accompanying this file. This file is distributed on an "AS IS"
+# BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, express or implied. See the License for
+# the specific language governing permissions and limitations under the License.
 from typing import List
 
 import torch
-from scheduler.static_methods import merge_tensors
+from __future__ import annotations
 
 from djl_python.scheduler.batch import Batch
 
@@ -12,54 +22,25 @@ PAD_TOKEN_ID = 220
 
 class ContrastiveBatch(Batch):
 
-    def __init__(self, seq_dim_order: List[int], past_output_ids: torch.Tensor = None,
-                 past_attention_mask: torch.Tensor = None, past_hidden_states: torch.Tensor = None,
-                 logits: torch.Tensor = None, past_key_values: List[torch.Tensor] = None):
-        super().__init__(seq_dim_order, past_output_ids, past_attention_mask, logits, past_key_values)
+    def __init__(
+        self,
+        seq_dim_order: List[int],
+        past_output_ids: torch.Tensor = None,
+        past_attention_mask: torch.Tensor = None,
+        past_hidden_states: torch.Tensor = None,
+        logits: torch.Tensor = None,
+        past_key_values: List[torch.Tensor] = None,
+    ):
         self.past_hidden_states = past_hidden_states
+        self.logits = logits
 
-    def merge(self, batch: ContrastiveBatch, seq_delta):
-        past_output_ids = merge_tensors(self.past_output_ids,
-                                        batch.past_output_ids,
-                                        seq_delta=seq_delta,
-                                        seq_order=1,
-                                        is_pad_token=True)
-        past_attention_mask = merge_tensors(self.past_attention_mask,
-                                            batch.past_attention_mask,
-                                            seq_delta=seq_delta,
-                                            seq_order=1)
+        super().__init__(seq_dim_order,
+                         past_output_ids=past_output_ids,
+                         past_attention_mask=past_attention_mask,
+                         past_key_values=past_key_values)
 
-        past_hidden_states = merge_tensors(self.past_hidden_states,
-                                           batch.past_hidden_states,
-                                           seq_delta=seq_delta,
-                                           seq_order=1)
-        logits = merge_tensors(self.logits,
-                               batch.logits,
-                               seq_delta=seq_delta,
-                               seq_order=-1)
-
-        past_key_values = []
-        for i in range(len(self.past_key_values)):
-            past_key_values.append(
-                merge_tensors(self.past_key_values[i],
-                              batch.past_key_values[i],
-                              seq_delta=seq_delta,
-                              seq_order=2))
-
-        return ContrastiveBatch(seq_dim_order=self.seq_dim_order,
-                                past_output_ids=past_output_ids,
-                                past_attention_mask=past_attention_mask,
-                                past_hidden_states=past_hidden_states,
-                                logits=logits,
-                                past_key_values=past_key_values)
-
-    @staticmethod
-    def trim_tensor(trim_sequence: int, keep_indices: List[int], seq_dim_order,
-                    tensor: torch.Tensor):
+    def merge(self, batch: ContrastiveBatch, seq_delta) -> ContrastiveBatch:
         pass
 
     def trim(self, trim_sequence: int, keep_indices: List[int]):
         pass
-
-
-
--- engines/python/setup/djl_python/scheduler/seq_batcher.py
+++ engines/python/setup/djl_python/scheduler/seq_batcher.py
@@ -10,7 +10,6 @@
 # or in the "LICENSE.txt" file accompanying this file. This file is distributed on an "AS IS"
 # BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, express or implied. See the License for
 # the specific language governing permissions and limitations under the License.
-
 from __future__ import annotations
 
 from typing import Dict
@@ -28,78 +27,18 @@ class SeqBatcher(object):
         self.offsets = offsets
         self.exit_index_end_position = {}
 
-        past_key_values_size = batch.past_key_values[0][0].size()
-        self.batch_size = past_key_values_size[0]
-        self.seq_len = past_key_values_size[2]
-
     def get_batch(self) -> Batch:
         return self.batch
 
     def add_batch(self, new_seq_batcher: SeqBatcher):
-        seq_delta = self.seq_len - new_seq_batcher.seq_len
-        self.batch = self.batch.merge(new_seq_batcher.batch, seq_delta)
-
-        # update other batch control variables
-        self.batch_size = self.batch_size + new_seq_batcher.batch_size
-        self.request_uids = torch.cat(
-            [self.request_uids, new_seq_batcher.request_uids], dim=0)
-        self.offsets = torch.cat([self.offsets, new_seq_batcher.offsets - abs(seq_delta)],
-                                 dim=0)
-        self.seq_len = max(self.seq_len, new_seq_batcher.seq_len)
+        pass
 
     def exit_criteria(self, output_ids: torch.Tensor, max_length: int,
                       eos_token_id: int):
-        for i in range(len(output_ids)):
-            if self.seq_len - self.offsets[i] >= max_length or output_ids[i] == eos_token_id:
-                if i not in self.exit_index_end_position:
-                    self.exit_index_end_position[i] = self.seq_len
+        pass
 
     def collect_and_trim(self) -> Dict[int, torch.Tensor]:
-        if len(self.exit_index_end_position) == 0:
-            return None
-
-        finished_sequences = {}
-        exit_indices = set()
-
-        # Adds the finished requests to the finished_sequences
-        # Batch index is added to the set to be removed from batch later.
-        for batch_index, seq_end_position in self.exit_index_end_position.items(
-        ):
-            uid = self.request_uids[batch_index]
-            offset = self.offsets[batch_index]
-            output = self.batch.past_output_ids[batch_index,
-                                                offset:seq_end_position]
-            finished_sequences[uid] = output
-            exit_indices.add(batch_index)
-
-        # finding the row with non-finished sequences.
-        keep_indices = []
-        j = 0
-        for i in range(self.batch_size):
-            if i not in exit_indices:
-                keep_indices[j] = i
-                j += 1
-
-        # if all the requests finished generating sequences, then reset the batch and return
-        if len(keep_indices) == 0:
-            self.request_uids = torch.zeros([0, 1],
-                                            dtype=self.request_uids.dtype)
-            self.offsets = torch.zeros([0, 1], dtype=self.offsets.dtype)
-            self.batch = None
-            self.batch_size = 0
-            self.seq_len = 0
-            self.exit_index_end_position = None
-            return finished_sequences
-
-        self.request_uids = torch.index_select(
-            self.request_uids, 0,
-            torch.LongTensor(keep_indices)).resize(-1, 1)
-        self.offsets = torch.index_select(
-            self.offsets, 0, torch.LongTensor(keep_indices)).resize(-1, 1)
-        trim_sequence = torch.min(self.offsets, dim=0).tolist()[0]
-        self.offsets = torch.subtract(self.offsets, trim_sequence)
-
-        # Trim batch and sequence dimension if needed.
+        pass
 
     def seq_complete(self) -> bool:
-        return len(self.exit_index_end_position) > 0
+        pass
