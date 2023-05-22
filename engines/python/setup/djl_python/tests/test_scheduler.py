4fdb620efabe7c99a304a9ef4553c6ffce0f41db
sindhuso <somasundaram.sindhu@gmail.com>
2023-05-22 15:14:47 -0700

    torch in deps, add deps and assert, fix testcase

--- engines/python/setup/djl_python/tests/test_scheduler.py
+++ engines/python/setup/djl_python/tests/test_scheduler.py
@@ -4,6 +4,7 @@ from scheduler.seq_batch_scheduler_impl import GreedySeqBatchScheduler
 from scheduler.search_config import SearchConfig
 import torch
 
+
 class TestScheduler(unittest.TestCase):
 
     def test_lm_block(self):
@@ -17,6 +18,7 @@ class TestScheduler(unittest.TestCase):
         ]
 
         output0 = lm_block.forward(input0, None)
+        assert len(output0[1]) == GPTConfig().numLayers
 
         # input with kv_cache
         past_key_values = output0[1]
@@ -24,6 +26,9 @@ class TestScheduler(unittest.TestCase):
         past_seq = past_key_values[0][0].shape[-2]
         position_ids = torch.tensor([[past_seq]])
         attention_mask = torch.ones(past_seq + 1, dtype=torch.int64)
+        output1 = lm_block.forward([input_ids, position_ids, attention_mask],
+                                   past_key_values)
+        assert len(output1[1]) == GPTConfig().numLayers
         output1 = lm_block.forward([input_ids, position_ids, attention_mask],
                                    past_key_values)
 
