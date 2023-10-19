444e2a09918dda67cf71aadef9f279a27c86f0d2
KexinFeng <fenkexin@amazon.com>
2023-10-19 23:31:02 +0000

    generator

--- engines/python/setup/djl_python/tests/test_rolling_batch/lmi_nonflash.py
+++ engines/python/setup/djl_python/tests/test_rolling_batch/lmi_nonflash.py
@@ -7,38 +7,28 @@ new_path = os.path.normpath(os.path.join(script_directory, relative_path))
 sys.path.append(new_path)
 
 from djl_python.rolling_batch.lmi_dist_rolling_batch import LmiDistRollingBatch
+from djl_python.rolling_batch.scheduler_rolling_batch import SchedulerRollingBatch
 from djl_python.tests.test_rolling_batch.generator import Generator, print_rank0
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-"""
-option.model_id=TheBloke/Llama-2-13B-Chat-fp16
-option.tensor_parallel_degree=4
-option.dtype=fp16
-option.rolling_batch=auto
-option.max_rolling_batch_size=8
-option.model_loading_timeout=7200
-option.max_rolling_batch_prefill_tokens=36080
-"""
-properties = {"tensor_parallel_degree": 1,
-              "dtype": "fp16",
-              "max_rolling_batch_size": 8,
+
+properties = {"tensor_parallel_degree": 2,
+              "max_rolling_batch_size": 32,
               "model_loading_timeout": 7200,
-              "max_rolling_batch_prefill_tokens": 10000,
-              "paged_attention": "True"}
+              "dtype": "fp16"}
 
 # model_id = "TheBloke/Llama-2-13B-Chat-fp16"
 # model_id = "openlm-research/open_llama_7b_v2"
 model_id = "huggyllama/llama-7b"
+model_id = "bigscience/bloom-560m"
 
-
-"""
-{"inputs":"write a program to add two numbers in python","parameters":{"max_new_tokens":300, "do_sample":true, "temperature":0.001}}
-"""
+variable = "USE_FLASH_ATTENTION"
+assert variable in os.environ and os.environ[variable] == "false"
 
 # ===================== lmi ============================
 print("=========== before =========")
-# rolling_batch = SchedulerRollingBatch(model_id, device, properties)
-rolling_batch = LmiDistRollingBatch(model_id, device, properties)
+rolling_batch = SchedulerRollingBatch(model_id, device, properties)
+# rolling_batch = LmiDistRollingBatch(model_id, device, properties)
 rolling_batch.output_formatter = None
 print("reach here")
 
@@ -64,7 +54,7 @@ for _ in range(7):
 
 
 print('========== inference_infty ===========')
-gen.step(step=500)
-for req_id, out in gen.output_all.items():
-    print_rank0(f"\n====req_id: {req_id}=====\n{gen.input_all[req_id][0] + ''.join(out)}\n")
+# gen.step(step=500)
+# for req_id, out in gen.output_all.items():
+#     print_rank0(f"\n====req_id: {req_id}=====\n{gen.input_all[req_id][0] + ''.join(out)}\n")
 
