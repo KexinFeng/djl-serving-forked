2a74e1c8e7cf8168107652af8a5a12d332781d93
KexinFeng <fenkexin@amazon.com>
2023-12-27 23:03:46 +0000

    specdec_dev1

--- engines/python/setup/djl_python/rolling_batch/lmi_dist_rolling_batch.py
+++ engines/python/setup/djl_python/rolling_batch/lmi_dist_rolling_batch.py
@@ -157,7 +157,7 @@ class LmiDistRollingBatch(RollingBatch):
         # prefill step
         if new_batch:
             batch = new_batch
-            generations, next_batch = self.model.generate_token(batch)
+            generations, next_batch = self.model.generate_token(batch, draft_model=self.draft_model.model, spec_length=self.properties.get("spec_length", 0))
             if next_batch is not None:
                 self.cache[next_batch.batch_id] = next_batch
         else:
@@ -168,7 +168,7 @@ class LmiDistRollingBatch(RollingBatch):
                 batch = self.model.batch_type.concatenate(batches)
             else:
                 batch = batches[0]
-            generations, next_batch = self.model.generate_token(batch)
+            generations, next_batch = self.model.generate_token(batch, draft_model=self.draft_model.model, spec_length=self.properties.get("spec_length", 0))
             if next_batch is not None:
                 self.cache[next_batch.batch_id] = next_batch
 
@@ -192,9 +192,10 @@ class LmiDistRollingBatch(RollingBatch):
                 if isinstance(log_prob, torch.Tensor):
                     log_prob = log_prob.item()
                 token = Token(
-                    token_id, ""
-                    if generation.token_is_special else generation.token_text,
-                    log_prob, generation.token_is_special)
+                    token_id, 
+                    generation.token_text_no_special,
+                    log_prob, 
+                    generation.token_is_special)
                 request.set_next_token(token,
                                        self.output_formatter,
                                        last_token=is_last_token,
@@ -242,8 +243,12 @@ class LmiDistRollingBatch(RollingBatch):
             self.batch_id_counter += 1
 
             return self.batch_cls.get_batch(
-                batch, self.model.config, self.model.tokenizer,
+                batch, 
+                self.model.config, 
+                self.model.tokenizer,
                 kwargs.get("torch_dtype", torch.float16),
-                torch.device(f"cuda:{self.model.device.index}"))
+                torch.device(f"cuda:{self.model.device.index}"),
+                # spec_dec parameters
+                self.properties.get("spec_length", 0))
         else:
             return None
--- engines/python/setup/djl_python/tests/rolling_batch_test_scripts/lmi-specdec.py
+++ engines/python/setup/djl_python/tests/rolling_batch_test_scripts/lmi-specdec.py
@@ -30,9 +30,10 @@ properties = {"mpi_mode": "true",
               "max_rolling_batch_size": 28,
               "model_loading_timeout": 3600,
               "max_rolling_batch_prefill_tokens": 1000,
-              "paged_attention": "True"}
+              "paged_attention": "True",
+              "spec_length": 8}
 
-model_id = "TheBloke/Llama-2-13B-Chat-fp16"  # multi gpu; 7,236 MiBx4 
+# model_id = "TheBloke/Llama-2-13B-Chat-fp16"  # multi gpu; 7,236 MiBx4 
 # model_id = "openlm-research/open_llama_7b_v2"
 
 # model_id = "huggyllama/llama-7b"  # 9,542MiB / 23,028MiB;
@@ -42,13 +43,12 @@ model_id = "TheBloke/Llama-2-13B-Chat-fp16"  # multi gpu; 7,236 MiBx4
 # model_id = "gpt2"
 # model_id = "facebook/opt-125m"
 
-# model_id = "TheBloke/Llama-2-7B-Chat-fp16"  # 14,114MiB / 23,028MiB
-# draft_model_id = "TinyLlama/TinyLlama-1.1B-Chat-v0.6"  #  2,710MiB / 23,028MiB
+model_id = "TheBloke/Llama-2-7B-Chat-fp16"  # 14,114MiB / 23,028MiB
+draft_model_id = "TinyLlama/TinyLlama-1.1B-Chat-v0.6"  #  2,710MiB / 23,028MiB
 # weight model.layers.0.self_attn.rotary_emb.inv_freq does not exist
 # model_id = "TinyLlama/TinyLlama-1.1B-python-v0.1"
 # model_id = "codellama/CodeLlama-7b-hf"  # 14,054MiB / 23028MiB;
 
-draft_model_id = None
 
 # ===================== lmi ============================
 print("=========== before =========")
