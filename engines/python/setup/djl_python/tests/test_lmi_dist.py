ec7e09d7d152afa4831ecbec164c2e73313591c9
KexinFeng <fenkexin@amazon.com>
2024-01-19 19:40:07 +0000

    spec_len = 5

--- engines/python/setup/djl_python/tests/test_lmi_dist.py
+++ engines/python/setup/djl_python/tests/test_lmi_dist.py
@@ -74,11 +74,11 @@ class TestLmiDist(unittest.TestCase):
 
         # --- Models ---
         model_names = [
-            # "TheBloke/Llama-2-13B-Chat-fp16",
-            "TheBloke/Llama-2-7B-Chat-fp16",
+            "TheBloke/Llama-2-13B-Chat-fp16",
+            # "TheBloke/Llama-2-7B-Chat-fp16",
             # TODO: fix this. weight model.layers.0.self_attn.rotary_emb.inv_freq does not exist
             # "TheBloke/Llama-2-7B-Chat-AWQ",
-            "TinyLlama/TinyLlama-1.1B-Chat-v0.6",
+            # "TinyLlama/TinyLlama-1.1B-Chat-v0.6",
             # TODO: fix this. weight model.layers.0.self_attn.rotary_emb.inv_freq does not exist
             # "TinyLlama/TinyLlama-1.1B-python-v0.1",
             # g5.12xlarge single gpu ok. But no way to clear the gpu memory after running llama-2-7b thus cause OOM
@@ -98,7 +98,7 @@ class TestLmiDist(unittest.TestCase):
             draft_model_id = model_id
             # draft_model_id = None
 
-            properties['spec_length'] = 1
+            properties['spec_length'] = 5
             properties["draft_model_id"] = draft_model_id
 
             # ===================== lmi_dist ============================
